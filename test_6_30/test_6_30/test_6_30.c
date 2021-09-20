952a25af22976d8281dc25582be2c0c913390f53
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-09-20 21:08:51 +0800

    Add files via upload

---
+++ test_6_30/test_6_30/test_6_30.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int n = 6;
+//	int i, j, m;
+//	for (i = 1,m = 0; i <= 6; i++, m++)
+//	{
+//		for (j = i; j <= 6; j++)
+//		{
+//			printf("%-2d ", (j) * ((j) + 1) / 2 - m);
+//		}
+//		printf("\n");
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+int main()
+{
+
+
+	return 0;
+}
